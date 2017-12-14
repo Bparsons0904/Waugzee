f4867bd6b48661277cc29f735f08fe07dea41c4f
Unknown <Deadstylebp@gmail.com>
2017-12-13 22:26:42 -0600

    User Profile Model created

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', login, {'template_name': 'users/login.html'}, name='login'),
     path('logout/', views.logout_view, name='logout'),
     # path('profile/', views.update_profile, name='profile'),
-    path('signup/', views.signup, name='signup'),
+    # path('signup/', views.signup, name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, login, authenticate
-from .forms import SignUpForm
+from django import forms
 
 def index(request):
     return render(request, 'lists/index.html')
@@ -8,19 +8,3 @@ def index(request):
 def logout_view(request):
     logout(request)
     return render(request, 'lists/index.html')
-
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.birth_date = form.cleaned_data.get('birth_date')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = SignUpForm()
-    return render(request, 'users/signup.html', {'form': form})
